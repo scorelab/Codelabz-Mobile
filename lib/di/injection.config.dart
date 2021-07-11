6628122785eebb45d9ff2512bfde5b83c567a078
Hirantha <mail@hirantha.xyz>
2021-07-11 21:41:33 +0530

    dependency injections

---
+++ lib/di/injection.config.dart
@@ -0,0 +1,27 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+// **************************************************************************
+// InjectableConfigGenerator
+// **************************************************************************
+
+import 'package:cloud_firestore/cloud_firestore.dart' as _i4;
+import 'package:firebase_auth/firebase_auth.dart' as _i3;
+import 'package:get_it/get_it.dart' as _i1;
+import 'package:google_sign_in/google_sign_in.dart' as _i5;
+import 'package:injectable/injectable.dart' as _i2;
+
+import 'registed_modules.dart' as _i6; // ignore_for_file: unnecessary_lambdas
+
+// ignore_for_file: lines_longer_than_80_chars
+/// initializes the registration of provided dependencies inside of [GetIt]
+_i1.GetIt $initGetIt(_i1.GetIt get,
+    {String? environment, _i2.EnvironmentFilter? environmentFilter}) {
+  final gh = _i2.GetItHelper(get, environment, environmentFilter);
+  final registerModules = _$RegisterModules();
+  gh.lazySingleton<_i3.FirebaseAuth>(() => registerModules.auth);
+  gh.lazySingleton<_i4.FirebaseFirestore>(() => registerModules.firestore);
+  gh.lazySingleton<_i5.GoogleSignIn>(() => registerModules.googleSignIn);
+  return get;
+}
+
+class _$RegisterModules extends _i6.RegisterModules {}
--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,11 @@
+import 'package:codelabz/di/injection.dart';
 import 'package:codelabz/presentation/codelabz_app.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
-void main() {
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  configureDependencies();
   runApp(CodeLabzApp());
 }
