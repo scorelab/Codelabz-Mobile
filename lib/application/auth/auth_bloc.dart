5295f6f30260129f37da0c423a85959fdc4be185
Hirantha <mail@hirantha.xyz>
2021-07-14 04:55:34 +0530

    auth domain

--- lib/application/auth/auth_bloc.dart
+++ lib/application/auth/auth_bloc.dart
@@ -1,6 +1,8 @@
 import 'dart:async';
 
 import 'package:bloc/bloc.dart';
+import 'package:codelabz/domain/auth/auth_repository.dart';
+import 'package:codelabz/domain/models/user.dart';
 import 'package:freezed_annotation/freezed_annotation.dart';
 import 'package:injectable/injectable.dart';
 
@@ -10,12 +12,25 @@ part 'auth_state.dart';
 
 @Injectable()
 class AuthBloc extends Bloc<AuthEvent, AuthState> {
-  AuthBloc() : super(const AuthState.initial());
+  final AuthRepository _authRepository;
+  AuthBloc(this._authRepository) : super(const AuthState.initial());
 
   @override
   Stream<AuthState> mapEventToState(
     AuthEvent event,
   ) async* {
-    // TODO: implement mapEventToState
+    yield* event.map(
+      authCheckRequested: (e) async* {
+        final userOption = await _authRepository.getSignedInUser();
+        yield userOption.fold(
+          () => const AuthState.unauthenticated(),
+          (user) => AuthState.authenticated(user),
+        );
+      },
+      signedOut: (e) async* {
+        await _authRepository.signout();
+        yield const AuthState.unauthenticated();
+      },
+    );
   }
 }
