8e8464ff2913ed3a86eed5f0d0e013c7ecf7c810
Hirantha <mail@hirantha.xyz>
2021-07-19 07:39:43 +0530

    fixed ui issues

--- lib/presentation/login/login_screen.dart
+++ lib/presentation/login/login_screen.dart
@@ -15,6 +15,7 @@ class LoginScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      resizeToAvoidBottomInset: false,
       appBar: AppBar(
         centerTitle: true,
         backgroundColor: Colors.white,
@@ -24,202 +25,194 @@ class LoginScreen extends StatelessWidget {
         ),
       ),
       body: SafeArea(
-        child: SingleChildScrollView(
-          child: Container(
-            padding: const EdgeInsets.all(20),
-            child: Column(
-              children: [
-                const SizedBox(height: 50),
-                BlocConsumer<LoginBloc, LoginState>(
-                  listener: (context, state) {
-                    state.authFailureOrSuccessOption.fold(
-                      () => {},
-                      (either) => either.fold(
-                        (failure) => {
-                          ScaffoldMessenger.of(context).showSnackBar(
-                            SnackBar(
-                              content: Text(
-                                failure.maybeMap(
-                                  cancelledByUser: (_) => "Cancelled!",
-                                  serverError: (_) => "Server Error!",
-                                  invalidEmailAndPasswordCombination: (_) =>
-                                      "Invalid email and password combination",
-                                  orElse: () => "",
-                                ),
+        child: Container(
+          padding: const EdgeInsets.all(20),
+          child: Column(
+            children: [
+              const SizedBox(height: 50),
+              BlocConsumer<LoginBloc, LoginState>(
+                listener: (context, state) {
+                  state.authFailureOrSuccessOption.fold(
+                    () => {},
+                    (either) => either.fold(
+                      (failure) => {
+                        ScaffoldMessenger.of(context).showSnackBar(
+                          SnackBar(
+                            content: Text(
+                              failure.maybeMap(
+                                cancelledByUser: (_) => "Cancelled!",
+                                serverError: (_) => "Server Error!",
+                                invalidEmailAndPasswordCombination: (_) =>
+                                    "Invalid email and password combination",
+                                orElse: () => "",
                               ),
                             ),
-                          )
-                        },
-                        (_) => {
-                          CodeLabzApp.router.navigateTo(context, Routes.home,
-                              clearStack: true),
-                          getIt<AuthBloc>()
-                              .add(const AuthEvent.authCheckRequested())
-                        },
-                      ),
-                    );
-                  },
-                  builder: (context, state) {
-                    return Form(
-                      autovalidateMode: state.showErrorMessages
-                          ? AutovalidateMode.onUserInteraction
-                          : AutovalidateMode.disabled,
-                      child: Column(
-                        children: [
-                          TextFormField(
-                            decoration: const InputDecoration(
-                              border: OutlineInputBorder(),
-                              prefixIcon: Icon(Icons.email, color: Colors.grey),
-                              labelText: 'Email',
-                            ),
-                            autocorrect: false,
-                            onChanged: (text) =>
-                                Provider.of<LoginBloc>(context, listen: false)
-                                    .add(LoginEvent.emailChanged(text)),
-                            validator: (_) => Provider.of<LoginBloc>(context)
-                                .state
-                                .emailAddress
-                                .value
-                                .fold(
-                                  (f) => f.maybeMap(
-                                    invalidEmail: (_) => "Invalid Email",
-                                    orElse: () => null,
-                                  ),
-                                  (_) => null,
-                                ),
                           ),
-                          const SizedBox(height: 5),
-                          TextFormField(
-                            obscureText: !state.showPassword,
-                            decoration: InputDecoration(
-                              border: const OutlineInputBorder(),
-                              prefixIcon:
-                                  const Icon(Icons.lock, color: Colors.grey),
-                              suffixIcon: IconButton(
-                                onPressed: () => Provider.of<LoginBloc>(context,
-                                        listen: false)
-                                    .add(const LoginEvent
-                                        .togglePasswordVisibility()),
-                                icon: Icon(
-                                  state.showPassword
-                                      ? FontAwesomeIcons.eye
-                                      : FontAwesomeIcons.eyeSlash,
+                        )
+                      },
+                      (_) => {
+                        CodeLabzApp.router
+                            .navigateTo(context, Routes.home, clearStack: true),
+                        getIt<AuthBloc>()
+                            .add(const AuthEvent.authCheckRequested())
+                      },
+                    ),
+                  );
+                },
+                builder: (context, state) {
+                  return Form(
+                    autovalidateMode: state.showErrorMessages
+                        ? AutovalidateMode.onUserInteraction
+                        : AutovalidateMode.disabled,
+                    child: Column(
+                      children: [
+                        TextFormField(
+                          decoration: const InputDecoration(
+                            border: OutlineInputBorder(),
+                            prefixIcon: Icon(Icons.email, color: Colors.grey),
+                            labelText: 'Email',
+                          ),
+                          autocorrect: false,
+                          onChanged: (text) =>
+                              Provider.of<LoginBloc>(context, listen: false)
+                                  .add(LoginEvent.emailChanged(text)),
+                          validator: (_) => Provider.of<LoginBloc>(context)
+                              .state
+                              .emailAddress
+                              .value
+                              .fold(
+                                (f) => f.maybeMap(
+                                  invalidEmail: (_) => "Invalid Email",
+                                  orElse: () => null,
                                 ),
+                                (_) => null,
                               ),
-                              labelText: 'Password',
-                            ),
-                            onChanged: (text) =>
-                                Provider.of<LoginBloc>(context, listen: false)
-                                    .add(LoginEvent.passwordChanged(text)),
-                            validator: (_) => Provider.of<LoginBloc>(context)
-                                .state
-                                .password
-                                .value
-                                .fold(
-                                  (f) => f.maybeMap(
-                                    shortPassword: (_) => "Short Password",
-                                    orElse: () => null,
-                                  ),
-                                  (_) => null,
-                                ),
-                          ),
-                          const SizedBox(height: 20),
-                          SizedBox(
-                            width: double.maxFinite,
-                            child: TextButton(
-                              onPressed: state.isSubmitting
-                                  ? null
-                                  : () => _onClickLogin(context),
-                              style: ButtonStyle(
-                                backgroundColor:
-                                    MaterialStateProperty.all<Color>(
-                                        state.isSubmitting
-                                            ? Colors.grey
-                                            : Colors.blueAccent),
-                                foregroundColor:
-                                    MaterialStateProperty.all(Colors.white),
+                        ),
+                        const SizedBox(height: 5),
+                        TextFormField(
+                          obscureText: !state.showPassword,
+                          decoration: InputDecoration(
+                            border: const OutlineInputBorder(),
+                            prefixIcon:
+                                const Icon(Icons.lock, color: Colors.grey),
+                            suffixIcon: IconButton(
+                              onPressed: () =>
+                                  Provider.of<LoginBloc>(context, listen: false)
+                                      .add(const LoginEvent
+                                          .togglePasswordVisibility()),
+                              icon: Icon(
+                                state.showPassword
+                                    ? FontAwesomeIcons.eye
+                                    : FontAwesomeIcons.eyeSlash,
                               ),
-                              child: const Text("Log in"),
                             ),
+                            labelText: 'Password',
                           ),
-                          Row(
-                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                            children: [
-                              Container(
-                                  height: 1,
-                                  color: Colors.grey,
-                                  width:
-                                      MediaQuery.of(context).size.width * 0.4),
-                              const Text(
-                                "or",
-                                style: TextStyle(
-                                    fontWeight: FontWeight.bold, fontSize: 20),
-                              ),
-                              Container(
-                                  height: 1,
-                                  color: Colors.grey,
-                                  width:
-                                      MediaQuery.of(context).size.width * 0.4),
-                            ],
-                          ),
-                          LoginButton(
-                            assetName: "assets/icons/google.png",
-                            text: "Signin with Google",
-                            onPress: () => _signInWithGoogle(context),
-                            disabled: state.isSubmitting,
-                          ),
-                          LoginButton(
-                            assetName: "assets/icons/facebook.png",
-                            text: "Signin with Facebook",
-                            onPress: () => _signInwithFacebook(context),
-                            disabled: state.isSubmitting,
-                          ),
-                          LoginButton(
-                            assetName: "assets/icons/github.png",
-                            text: "Signin with Github",
-                            onPress: () => _signInwithFacebook(context),
-                            disabled: state.isSubmitting,
-                          ),
-                          LoginButton(
-                            assetName: "assets/icons/twitter.png",
-                            text: "Signin with Twitter",
-                            onPress: () => _signInwithFacebook(context),
-                            disabled: state.isSubmitting,
-                          ),
-                          const SizedBox(height: 30),
-                          RichText(
-                            text: TextSpan(
-                              children: <TextSpan>[
-                                const TextSpan(
-                                    text: "New to ",
-                                    style: TextStyle(color: Colors.black87)),
-                                const TextSpan(
-                                    text: "CodeLabz",
-                                    style: TextStyle(
-                                      color: Colors.black,
-                                      fontWeight: FontWeight.bold,
-                                    )),
-                                const TextSpan(
-                                    text: "? ",
-                                    style: TextStyle(color: Colors.black87)),
-                                TextSpan(
-                                  text: "Create an account",
-                                  style: const TextStyle(color: Colors.black87),
-                                  recognizer: TapGestureRecognizer()
-                                    ..onTap =
-                                        () => _onClickCreateAccount(context),
+                          onChanged: (text) =>
+                              Provider.of<LoginBloc>(context, listen: false)
+                                  .add(LoginEvent.passwordChanged(text)),
+                          validator: (_) => Provider.of<LoginBloc>(context)
+                              .state
+                              .password
+                              .value
+                              .fold(
+                                (f) => f.maybeMap(
+                                  shortPassword: (_) => "Short Password",
+                                  orElse: () => null,
                                 ),
-                              ],
+                                (_) => null,
+                              ),
+                        ),
+                        const SizedBox(height: 20),
+                        SizedBox(
+                          width: double.maxFinite,
+                          child: TextButton(
+                            onPressed: state.isSubmitting
+                                ? null
+                                : () => _onClickLogin(context),
+                            style: ButtonStyle(
+                              backgroundColor: MaterialStateProperty.all<Color>(
+                                  state.isSubmitting
+                                      ? Colors.grey
+                                      : Colors.blueAccent),
+                              foregroundColor:
+                                  MaterialStateProperty.all(Colors.white),
                             ),
+                            child: const Text("Log in"),
                           ),
-                          const SizedBox(height: 10),
-                        ],
-                      ),
-                    );
-                  },
+                        ),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Container(
+                                height: 1,
+                                color: Colors.grey,
+                                width: MediaQuery.of(context).size.width * 0.4),
+                            const Text(
+                              "or",
+                              style: TextStyle(
+                                  fontWeight: FontWeight.bold, fontSize: 20),
+                            ),
+                            Container(
+                                height: 1,
+                                color: Colors.grey,
+                                width: MediaQuery.of(context).size.width * 0.4),
+                          ],
+                        ),
+                        LoginButton(
+                          assetName: "assets/icons/google.png",
+                          text: "Signin with Google",
+                          onPress: () => _signInWithGoogle(context),
+                          disabled: state.isSubmitting,
+                        ),
+                        LoginButton(
+                          assetName: "assets/icons/facebook.png",
+                          text: "Signin with Facebook",
+                          onPress: () => _signInwithFacebook(context),
+                          disabled: state.isSubmitting,
+                        ),
+                        LoginButton(
+                          assetName: "assets/icons/github.png",
+                          text: "Signin with Github",
+                          onPress: () => _signInwithFacebook(context),
+                          disabled: state.isSubmitting,
+                        ),
+                        LoginButton(
+                          assetName: "assets/icons/twitter.png",
+                          text: "Signin with Twitter",
+                          onPress: () => _signInwithFacebook(context),
+                          disabled: state.isSubmitting,
+                        ),
+                      ],
+                    ),
+                  );
+                },
+              ),
+              const Spacer(),
+              RichText(
+                text: TextSpan(
+                  children: <TextSpan>[
+                    const TextSpan(
+                        text: "New to ",
+                        style: TextStyle(color: Colors.black87)),
+                    const TextSpan(
+                        text: "CodeLabz",
+                        style: TextStyle(
+                          color: Colors.black,
+                          fontWeight: FontWeight.bold,
+                        )),
+                    const TextSpan(
+                        text: "? ", style: TextStyle(color: Colors.black87)),
+                    TextSpan(
+                      text: "Create an account",
+                      style: const TextStyle(color: Colors.black87),
+                      recognizer: TapGestureRecognizer()
+                        ..onTap = () => _onClickCreateAccount(context),
+                    ),
+                  ],
                 ),
-              ],
-            ),
+              ),
+            ],
           ),
         ),
       ),
