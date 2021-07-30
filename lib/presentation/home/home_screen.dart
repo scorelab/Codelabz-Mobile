c9d9e2f740d9bb596f985bc051692367af56241a
Hirantha <mail@hirantha.xyz>
2021-07-30 23:55:23 +0530

    home bottom navigation

--- lib/presentation/home/home_screen.dart
+++ lib/presentation/home/home_screen.dart
@@ -1,12 +1,63 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:persistent_bottom_nav_bar/persistent-tab-view.dart';
 
 class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: SafeArea(
-        child: Text("Home"),
+    return SafeArea(
+      child: Scaffold(
+        body: PersistentTabView(
+          context,
+          screens: [
+            Container(
+              color: Colors.red,
+            ),
+            Container(),
+            Container(),
+            Container(),
+          ],
+          items: _navBarsItems(),
+          decoration: NavBarDecoration(
+            borderRadius: BorderRadius.circular(10.0),
+            colorBehindNavBar: Colors.white,
+          ),
+          itemAnimationProperties: const ItemAnimationProperties(
+            duration: Duration(milliseconds: 200),
+            curve: Curves.ease,
+          ),
+          screenTransitionAnimation: const ScreenTransitionAnimation(
+            animateTabTransition: true,
+          ),
+          navBarStyle: NavBarStyle.style4,
+        ),
       ),
     );
+  }
+
+  List<PersistentBottomNavBarItem> _navBarsItems() {
+    return [
+      PersistentBottomNavBarItem(
+        icon: const Icon(FontAwesomeIcons.home),
+        title: "Home",
+        inactiveColorPrimary: Colors.grey,
+      ),
+      PersistentBottomNavBarItem(
+        icon: const Icon(FontAwesomeIcons.building),
+        title: "Organizations",
+        inactiveColorPrimary: Colors.grey,
+      ),
+      PersistentBottomNavBarItem(
+        icon: const Icon(FontAwesomeIcons.graduationCap),
+        title: "Tutorials",
+        inactiveColorPrimary: Colors.grey,
+      ),
+      PersistentBottomNavBarItem(
+        icon: const Icon(FontAwesomeIcons.cog),
+        title: "Settings",
+        inactiveColorPrimary: Colors.grey,
+      ),
+    ];
   }
 }
